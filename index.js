a9b363b68e05cb62016fe5d269615295f81adc4c
hamza <azamh64@yahoo.com>
2022-01-15 13:18:21 +0500

    parsed csv data using csv parse package

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fs = require("fs");
 const results = [];
 
 fs.createReadStream("./kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    console.log(data);
     results.push(data);
   })
   .on("error", (err) => {
